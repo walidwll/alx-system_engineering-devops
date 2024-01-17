0b2418ec6be1fae265bb44325b065b412f1aa981
Walid Oualili <walid.oualili@gmail.com>
2024-01-16 23:47:15 -0800

    fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,38 +5,12 @@ import sys
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx status codes)
-
-        # Check if the response contains valid JSON data
-        if response.text.strip():  # Check if the response is not empty
-            data = response.json().get("data")
-
-            # Check if "subscribers" key is present in the JSON data
-            if data and "subscribers" in data:
-                return data["subscribers"]
-            else:
-                print("Error: Invalid JSON format - 'subscribers' key not found.")
-                return 0
-        else:
-            print("Error: Empty response or invalid JSON format.")
-            return 0
-
-    except requests.exceptions.HTTPError as err:
-        if err.response.status_code == 404:
-            # Subreddit not found (invalid subreddit)
-            return 0
-        else:
-            # Other HTTP error occurred
-            print(f"HTTP Error: {err}")
-            return 0
-    except requests.exceptions.RequestException as e:
-        # Other non-HTTP-related error occurred
-        print(f"Error: {e}")
+    response = requests.get(url,headers=headers)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
